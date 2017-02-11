c3d7da7a8226992a2d6ff66779857c7888cd0cca
vpumeyyv <vpumeyyv@venera.com>
2017-02-11 23:54:33 +0200

    project

---
+++ app/src/main/java/com/venera/homeapp/MapsActivity.java
@@ -0,0 +1,74 @@
+package com.venera.homeapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //
+    private GoogleMap mMap;
+    //
+    private LocationManager locationManager;
+    //Defining the required component for online location monitoring
+    private LocationListener listener;
+    //Defining the required component for any and all Bluetooth activity
+    private BluetoothAdapter mBluetoothAdapter;
+    //Defining a popup window to turn on Bluetooth
+    private Intent enableBtIntent;
+
+
+
+    @Override
+    //What hapens when I open the aplication is executed in 'onCreate'
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //Finding the device's bluetooth ID, if any.
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+        // Device does not support Bluetooth
+        }
+
+        //If Bluetooth isn't on, request user to turn it on using popup
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+
+
+    }
+}
