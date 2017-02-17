e8862bddb7e96d7fc0b2fdd36343c555fccd0366
vpumeyyv <vpumeyyv@venera.com>
2017-02-17 11:14:16 +0200

    Update MapsActivity.java

--- AndroidApp/app/src/main/java/com/venera/homeapp/MapsActivity.java
+++ AndroidApp/app/src/main/java/com/venera/homeapp/MapsActivity.java
@@ -242,6 +242,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         CircleOptions circleOptions = new CircleOptions()
                 .center(test)
                 .clickable(true)
+                .anchor(1,0)
                 .radius(400)
                 .strokeWidth(0)
                 .fillColor(Color.argb(64,0,0,255));
@@ -251,7 +252,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(test)
                 .alpha(0)
                 .title("Testing...")
-                .snippet("It seems to work!"));
+                .snippet(readMsg));
 
         Test =mMap.addCircle(circleOptions);
 
